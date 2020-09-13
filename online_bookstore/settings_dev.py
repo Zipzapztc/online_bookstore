0b49e07c58f85ff11efb82b8a3387d0478ba5ad9
Zipzapztc <Zhuangtc_nb@foxmail.com>
2020-09-13 15:43:19 +0800

    更换sqlite3

--- online_bookstore/settings_dev.py
+++ online_bookstore/settings_dev.py
@@ -83,12 +83,26 @@ WSGI_APPLICATION = 'online_bookstore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'online_bookstore_db',
+        'USER': 'ztc',
+        'PASSWORD': os.environ['DATABASE_PASSWORD'],
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+
 
 
 # Password validation
--- online_bookstore/settings_prod.py
+++ online_bookstore/settings_prod.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'online_bookstore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'online_bookstore_db',
+        'USER': 'ztc',
+        'PASSWORD': os.environ['DATABASE_PASSWORD'],
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
